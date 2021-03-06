18db3675a9c8fb02c759fce923eb08c4a45dd314
unknown <b0gdan.munt3@gmail.com>
2021-03-06 13:15:30 +0200

    seminar2 v2

--- seminar2/src/ro/ase/cts/program/Program.java
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -1,19 +1,27 @@
 package ro.ase.cts.program;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.AplicantReadable;
+import ro.ase.cts.clase.readers.ElevReader;
+import ro.ase.cts.clase.readers.StudentReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Program {
 
+	public static List<Aplicant> citesteAplicant(String numeFisier, AplicantReadable aplicantReadable) throws FileNotFoundException{
+		return aplicantReadable.readAplicant(numeFisier);
+	}
+
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAplicanti;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			listaAplicanti = citesteAplicant("elevi.txt", new ElevReader());
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
