55b46a8df51bd8aaa9966f0e0203e5f8400db02f
unknown <b0gdan.munt3@gmail.com>
2021-03-13 13:28:06 +0200

    seminar 3 final

---
+++ seminar3/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,33 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Proiect;
+import ro.ase.cts.clase.readers.AplicantReadable;
+import ro.ase.cts.clase.readers.ElevReader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Program {
+
+	public static List<Aplicant> citesteAplicant(AplicantReadable aplicantReadable) throws FileNotFoundException{
+		return aplicantReadable.readAplicants();
+	}
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAplicanti;
+		try {
+			Proiect proiect = new Proiect(80);
+			listaAplicanti = citesteAplicant(new ElevReader("elevi.txt"));
+			for(Aplicant aplicant:listaAplicanti){
+				System.out.println(aplicant.toString());
+				System.out.println(aplicant.getFinantare());
+				aplicant.displayStateInProject(proiect);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
