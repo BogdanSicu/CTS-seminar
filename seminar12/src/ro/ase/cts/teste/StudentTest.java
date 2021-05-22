6bb6da6b2194a96041404ef38bb055892010b1eb
unknown <b0gdan.munt3@gmail.com>
2021-05-22 13:21:46 +0300

    seminar 12 v2

--- seminar12/src/ro/ase/cts/teste/StudentTest.java
+++ seminar12/src/ro/ase/cts/teste/StudentTest.java
@@ -37,5 +37,68 @@ public class StudentTest {
         assertEquals(nota, student.getNota(0));
     }
 
+    @Test
+    public void testGetNotaForMoreNota(){
+        Student student = new Student();
+        student.adaugaNota(5);
+        student.adaugaNota(9);
+        student.adaugaNota(7);
+        student.adaugaNota(3);
+        assertEquals(5, student.getNota(0));
+        assertEquals(9, student.getNota(1));
+        assertEquals(7, student.getNota(2));
+        assertEquals(3, student.getNota(3));
+    }
+
+    @Test
+    public void testAdaugaOSinguraNota(){
+        Student student = new Student();
+        int nota = 10;
+        student.adaugaNota(nota);
+        assertEquals(1, student.getNote().size());
+    }
+
+    @Test
+    public void testAdaugaNotaIncorecta(){
+        Student student = new Student();
+        try{
+            student.adaugaNota(-1);
+            fail("S-a introdus o valoare incorecta");
+        }catch(IllegalArgumentException e){
+
+        }catch(Exception e){
+            fail("Nu arunca o exceptie de tipul IllegalArgumentException");
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdaugaNotaIncorectaJunit4(){
+        Student student = new Student();
+        student.adaugaNota(-1);
+    }
+
+    @Test
+    public void testStudentAreRestanta(){
+        Student student = new Student();
+        student.adaugaNota(3);
+        boolean rezultat = student.areRestante();
+        assertTrue(rezultat);
+    }
+
+    @Test
+    public void testStudentNuAreRestanta(){
+        Student student = new Student();
+        student.adaugaNota(5);
+        boolean rezultat = student.areRestante();
+        assertFalse(rezultat);
+    }
+
+    @Test
+    public void testCalculeazaMedieCorect(){
+        Student student = new Student();
+        student.adaugaNota(4);
+        student.adaugaNota(5);
+        assertEquals(4.5, student.calculeazaMedie(), 0.0001);
+    }
 
 }
